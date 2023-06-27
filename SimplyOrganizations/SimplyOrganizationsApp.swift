63e720dccb774a859012f9739652d8c468fa6066
Kiran K <rogrdat@outlook.com>
2023-06-27 16:07:20 -0400

    Initial Commit

---
+++ SimplyOrganizations/SimplyOrganizationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplyOrganizationsApp.swift
+//  SimplyOrganizations
+//
+//  Created by Kiran Kari on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SimplyOrganizationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
